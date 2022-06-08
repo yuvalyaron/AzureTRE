eff931d8dfaa4497e208727e6575f862ba41b85f
Ben Coleman <benc.uk@gmail.com>
2022-06-08 10:49:26 +0100

    CLI PoC using Click and OpenAPI Tools (#1879)
    Standalone CLI investigation spike by @benc-uk 
    
    We're keeping the branch, however prioritizing the UI for now. 
    Thanks @benc-uk .

--- templates/workspaces/base/terraform/keyvault.tf
+++ templates/workspaces/base/terraform/keyvault.tf
@@ -3,7 +3,7 @@ resource "azurerm_key_vault" "kv" {
   location                 = azurerm_resource_group.ws.location
   resource_group_name      = azurerm_resource_group.ws.name
   sku_name                 = "standard"
-  purge_protection_enabled = var.keyvault_purge_protection_enabled
+  purge_protection_enabled = true
   tenant_id                = data.azurerm_client_config.current.tenant_id
 
   network_acls {
@@ -39,7 +39,6 @@ resource "azurerm_private_endpoint" "kvpe" {
   }
 }
 
-
 data "azurerm_user_assigned_identity" "resource_processor_vmss_id" {
   name                = "id-vmss-${var.tre_id}"
   resource_group_name = "rg-${var.tre_id}"
@@ -61,4 +60,28 @@ resource "azurerm_key_vault_access_policy" "deployer" {
   object_id    = data.azurerm_client_config.current.object_id
 
   secret_permissions = ["Get", "List", "Set", "Delete", "Purge"]
+}
+
+resource "null_resource" "wait_for_dns_vault" {
+  provisioner "local-exec" {
+    command    = "bash -c \"sleep 120s\""
+    on_failure = fail
+  }
+
+  triggers = {
+    always_run = azurerm_private_endpoint.kvpe.private_service_connection[0].private_ip_address # only wait on new/changed private IP address
+  }
+
+  depends_on = [azurerm_private_endpoint.kvpe]
+}
+
+resource "azurerm_key_vault_secret" "aad_tenant_id" {
+  name         = "auth-tenant-id"
+  value        = var.auth_tenant_id
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on = [
+    azurerm_key_vault_access_policy.deployer,
+    azurerm_key_vault_access_policy.resource_processor,
+    null_resource.wait_for_dns_vault
+  ]
 }
